459895599daab350103afcbfed01884b35fe7ff9
Seokjin Yoon <sjy5386@gmail.com>
2022-09-08 20:09:07 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-work_dir=$(pwd)
 minecraft_dir=~/minecraft
 server_jar=server.jar
-option=nogui
-
-jvm_heap_size=1024M
 
 while [ -f ${minecraft_dir}/${server_jar} ]
 do
-	cd ${minecraft_dir}
-	java -Xms${jvm_heap_size} -Xmx${jvm_heap_size} -jar ${server_jar} ${option}
-	cd ${work_dir}
+	docker-compose up
 	sh backup.sh
 done
