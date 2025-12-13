f523a1a91549df8053e5ca1da1a530fb1996a1bd
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-12-13 17:23:01 +0900

    Add new relic
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- main.sh
+++ main.sh
@@ -8,7 +8,11 @@ chmod +x ./backup.sh
 while [ -f ${MINECRAFT_DIR}/${SERVER_JAR} ]
 do
 	cd ${MINECRAFT_DIR}
-	java -Xms${JVM_HEAP_SIZE} -Xmx${JVM_HEAP_SIZE} -jar ${SERVER_JAR} ${OPTION}
+	if [ -f $NEWRELIC_AGENT ]; then
+		java -javaagent:$NEWRELIC_AGENT -Xms${JVM_HEAP_SIZE} -Xmx${JVM_HEAP_SIZE} -jar ${SERVER_JAR} ${OPTION}
+	else
+		java -Xms${JVM_HEAP_SIZE} -Xmx${JVM_HEAP_SIZE} -jar ${SERVER_JAR} ${OPTION}
+	fi
 	cd ${work_dir}
 	./backup.sh
 done
